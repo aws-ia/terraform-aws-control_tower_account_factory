4e94e1a7778d837dc51ed1042ff18bb7afb58b5b
AWS <aws>
2022-01-21 21:34:51 +0000

    Release: 1.1.1

--- modules/aft-account-request-framework/sqs.tf
+++ modules/aft-account-request-framework/sqs.tf
@@ -2,10 +2,11 @@ resource "aws_sqs_queue" "aft_account_request" {
   name                              = "aft-account-request.fifo"
   fifo_queue                        = true
   kms_master_key_id                 = aws_kms_alias.aft.name
+  visibility_timeout_seconds        = 240
   kms_data_key_reuse_period_seconds = 300
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.aft_account_request_dlq.arn
-    maxReceiveCount     = 1000
+    maxReceiveCount     = 1
   })
 }
 
