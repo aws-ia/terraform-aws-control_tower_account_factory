5c3124e3a922c1bdce0f0acf2125eb065074c2ab
Miguel <mlii@gft.com>
2022-02-23 13:59:30 +0000

    moved iam.tf to the terraform folder

---
+++ sources/aft-customizations-repos/aft-account-customizations/network-customizations/terraform/iam.tf
@@ -0,0 +1,8 @@
+data "aws_iam_policy" "network_administrator_policy" {
+  name = "NetworkAdministrator"
+}
+
+resource "aws_iam_role" "netops" {
+   name = "netops"
+   assume_role_policy = data.aws_iam_policy.network_administrator_policy.json     
+}
