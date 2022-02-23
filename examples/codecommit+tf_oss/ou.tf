549749365179cd4d54936113ae3d33a3479f2f92
Miguel <mlii@gft.com>
2022-02-23 09:05:04 +0000

    modified the parent_id reference

--- examples/codecommit+tf_oss/ou.tf
+++ examples/codecommit+tf_oss/ou.tf
@@ -4,5 +4,5 @@ data "aws_organizations_organization" "root" {}
 # Creating OUs for the different purposes (Network, services...)
 resource "aws_organizations_organizational_unit" "ou" {
   name      = "OUtesting" # name of the OU to create
-  parent_id = aws_organizations_organization.root.roots[0].id
+  parent_id = data.aws_organizations_organization.root.roots[0].id
 }
