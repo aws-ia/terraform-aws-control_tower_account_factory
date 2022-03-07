3ed96a065dc077cb77eea9938a77099b2a4400ea
Miguel <mlii@gft.com>
2022-03-07 16:14:01 +0000

    added multi account role creation

--- testing/aft-iam-roles/iam.tf
+++ testing/aft-iam-roles/iam.tf
@@ -15,41 +15,53 @@ provider "aws" {
   }
 }
 
-data "aws_caller_identity" "current" {}
 
-variable "aft_account_id" {
-  description = "AFT Management Account ID"
-  type        = string
-  default  = "947117813606"
-}
+data "aws_organizations_organization" "example" {}
+
+# data "aws_caller_identity" "current" {}
+
+# variable "aft_account_id" {
+#   description = "AFT Management Account ID"
+#   type        = string
+#   default     = "947117813606"
+# }
 
 ## Creation of the NetOps Role ##
+
 resource "aws_iam_role" "netops_role" {
+  depends_on = [data.aws_organizations_organization.example]
+  for_each = toset(data.aws_organizations_organization.example.non_master_accounts[*].id)
   provider = aws.aft_netops
   name     = "NetOpsAdmin"
   assume_role_policy = templatefile("${path.module}/iam_netops/netops_role_trust_policy.tpl",
     {
-      aft_account_id = var.aft_account_id
+      aft_account_id = each.value
     }
   )
 }
 
 resource "aws_iam_role_policy" "netops_policy" {
+  depends_on = [aws_iam_role.netops_role]
+  for_each = aws_iam_role.netops_role
   provider = aws.aft_netops
   name     = "NetOpsAdmin_policy"
-  role     = aws_iam_role.netops_role.id
+  role     = each.value.id
 
   policy = file("${path.module}/iam_netops/netops_role_policy.tpl")
 }
-## Creation of the NetOps Role ##
 
 module "netops_exec_role" {
+  depends_on = [aws_iam_role.netops_role]
+  for_each = aws_iam_role.netops_role
   source = "./netops-role"
   providers = {
     aws = aws.aft_netops
   }
-  trusted_entity = aws_iam_role.netops_role.arn
+  trusted_entity = each.value.arn
 }
+
+## Creation of the NetOps Role ##
+
 
 # module "log_archive_exec_role" {
 #   source = "./admin-role"
--- testing/aft-iam-roles/outputs.tf
+++ testing/aft-iam-roles/outputs.tf
@@ -1,9 +1,9 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-output "netops_role_arn" {
-  value = aws_iam_role.netops_role.arn
-}
+# output "netops_role_arn" {
+#   value = aws_iam_role.netops_role[each.value.arn]
+# }
 # output "ct_management_exec_role_arn" {
 #   value = module.ct_management_exec_role.arn
 # }
@@ -13,6 +13,10 @@ output "netops_role_arn" {
 # output "audit_exec_role_arn" {
 #   value = module.audit_exec_role.arn
 # }
-output "netops_execution_role_arn" {
-  value = module.netops_exec_role.arn
-}
+# output "netops_execution_role_arn" {
+#   value = module.netops_exec_role[each.value.arn]
+# }
+
+# output "account_ids" {
+#   value = data.aws_organizations_organization.example.non_master_accounts[*].id
+# }
