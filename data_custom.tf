f19505da0a754e819c468b9f62ea7bf688cb7b77
Zach Wolfgang <zachary.wolfgang@mdlz.com>
2022-05-16 11:22:18 -0400

    fix(secrets): use correct provider (#16)

--- data_custom.tf
+++ data_custom.tf
@@ -1,18 +1,22 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "aft_management" {
+  provider = aws.aft_management
+}
+
 data "aws_kms_secrets" "mondelez" {
+  provider = aws.aft_management
   secret {
     name    = "github_app_private_key"
-    payload = file("${path.module}/secrets/${data.aws_caller_identity.current.account_id}/github-app-private-key.pem.enc")
+    payload = file("${path.module}/secrets/${data.aws_caller_identity.aft_management.account_id}/github-app-private-key.pem.enc")
     context = {
-      AccountID = data.aws_caller_identity.current.account_id
+      AccountID = data.aws_caller_identity.aft_management.account_id
     }
   }
 
   secret {
     name    = "spacelift_api_key"
-    payload = file("${path.module}/secrets/${data.aws_caller_identity.current.account_id}/spacelift-api-key.enc")
+    payload = file("${path.module}/secrets/${data.aws_caller_identity.aft_management.account_id}/spacelift-api-key.enc")
     context = {
-      AccountID = data.aws_caller_identity.current.account_id
+      AccountID = data.aws_caller_identity.aft_management.account_id
     }
   }
 }
--- ssm_custom.tf
+++ ssm_custom.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "github_app_credentials" {
-  name = "/aft/config/github/app-credentials"
-  type = "SecureString"
+  provider = aws.aft_management
+  name     = "/aft/config/github/app-credentials"
+  type     = "SecureString"
   value = jsonencode({
     app_id          = 164830
     installation_id = 22274219
@@ -11,8 +12,9 @@ resource "aws_ssm_parameter" "github_app_credentials" {
 }
 
 resource "aws_ssm_parameter" "spacelift_api_credentials" {
-  name = "/aft/config/spacelift/api-credentials"
-  type = "SecureString"
+  provider = aws.aft_management
+  name     = "/aft/config/spacelift/api-credentials"
+  type     = "SecureString"
   value = jsonencode({
     api_key_id       = "01FX5W7Z54VYZTE8TYN0X3BJFB",
     api_key_secret   = data.aws_kms_secrets.mondelez.plaintext.spacelift_api_key
