12acd5add3ed9f22546120628838644448f889f7
Drew Arnold <drew.arnold@logicgate.com>
2023-06-13 15:49:06 -0500

    [GH-359] Update DynamoDB Tables from Provisioned to On-Demand

--- modules/aft-account-request-framework/ddb.tf
+++ modules/aft-account-request-framework/ddb.tf
@@ -2,11 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Table that stores account-meta data
-resource "aws_dynamodb_table" "aft_request_metadata" {
-  name           = "aft-request-metadata"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "id"
+resource "1112332" "aft_request_metadata" {
+  name         = "aft-request-metadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
@@ -53,8 +52,7 @@ resource "aws_dynamodb_table" "aft_request_metadata" {
 # Table that stores the configuration details for the account vending machine
 resource "aws_dynamodb_table" "aft_request" {
   name             = "aft-request"
-  read_capacity    = 1
-  write_capacity   = 1
+  billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "id"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
@@ -77,8 +75,7 @@ resource "aws_dynamodb_table" "aft_request" {
 # Table that stores the audit history for the account
 resource "aws_dynamodb_table" "aft_request_audit" {
   name             = "aft-request-audit"
-  read_capacity    = 1
-  write_capacity   = 1
+  billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "id"
   range_key        = "timestamp"
   stream_enabled   = true
@@ -107,8 +104,7 @@ resource "aws_dynamodb_table" "aft_request_audit" {
 # Table that stores the audit history for the account
 resource "aws_dynamodb_table" "aft_controltower_events" {
   name             = "aft-controltower-events"
-  read_capacity    = 5
-  write_capacity   = 5
+  billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "id"
   range_key        = "time"
   stream_enabled   = true
