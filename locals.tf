58196800a2c6b2c475d705b2443863cb82900c76
AWS <aws>
2025-04-15 20:59:03 +0000

    Release: 1.13.5

--- locals.tf
+++ locals.tf
@@ -34,4 +34,38 @@ locals {
     aft_tf_ddb_table_ssm_path                         = "/aft/config/oss-backend/table-id"
     aft_tf_version_ssm_path                           = "/aft/config/terraform/version"
   }
+  service_catalog_regional_availability = [
+    "ca-central-1",
+    "cn-north-1",
+    "cn-northwest-1",
+    "eu-north-1",
+    "eu-west-2",
+    "eu-west-3",
+    "us-east-1",
+    "us-east-2",
+    "us-gov-west-1",
+    "us-west-2",
+    "af-south-1",
+    "ap-east-1",
+    "ap-south-2",
+    "ap-southeast-2",
+    "ap-southeast-3",
+    "ap-southeast-4",
+    "me-central-1",
+    "me-south-1",
+    "sa-east-1",
+    "us-gov-east-1",
+    "ap-northeast-2",
+    "ap-northeast-3",
+    "ap-south-1",
+    "ap-southeast-1",
+    "eu-central-1",
+    "eu-south-1",
+    "eu-south-2",
+    "eu-west-1",
+    "il-central-1",
+    "us-west-1",
+    "ap-northeast-1",
+    "eu-central-2"
+  ]
 }
--- modules/aft-account-request-framework/lambda.tf
+++ modules/aft-account-request-framework/lambda.tf
@@ -63,7 +63,7 @@ resource "aws_lambda_function" "aft_account_request_action_trigger" {
   source_code_hash = var.request_framework_archive_hash
   memory_size      = 1024
   runtime          = var.lambda_runtime_python_version
-  timeout          = "300"
+  timeout          = "600"
   layers           = [var.aft_common_layer_arn]
 
   dynamic "vpc_config" {
