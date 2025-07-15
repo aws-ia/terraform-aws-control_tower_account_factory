ca2cfe8a3143b4e7fd790d486799b09268b8d57b
Cristian Sabaila <csabaila@geotogether.com>
2025-07-15 16:08:32 +0300

    fix the S3 AFT customizations key ID to use the ARN

--- modules/aft-customizations/s3.tf
+++ modules/aft-customizations/s3.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aft-codepipeline-
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = var.aft_kms_key_id
+      kms_master_key_id = var.aft_kms_key_arn
       sse_algorithm     = "aws:kms"
     }
   }
