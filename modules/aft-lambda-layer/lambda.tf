447727de500812e745d9d5df0c34bd5a0851ccb2
Hi6mu-Sa10 <hiromu.sato.x7b@ainj.co.jp>
2026-03-25 16:17:50 +0900

    fix: Explicitly define IAM permission dependencies before Lambda invocation
    related issue: https://github.com/aws-ia/terraform-aws-control_tower_account_factory/issues/614

--- modules/aft-lambda-layer/lambda.tf
+++ modules/aft-lambda-layer/lambda.tf
@@ -31,6 +31,12 @@ resource "aws_cloudwatch_log_group" "codebuild_trigger_loggroup" {
 data "aws_lambda_invocation" "trigger_codebuild_job" {
   function_name = aws_lambda_function.codebuild_trigger.function_name
 
+  depends_on = [
+    aws_iam_role.codebuild_trigger_lambda_role,
+    aws_iam_role_policy.codebuild_trigger_policy,
+    aws_iam_role_policy_attachment.codebuild_trigger_VPC_access
+  ]
+
   input = <<JSON
 {
   "codebuild_project_name": "${aws_codebuild_project.codebuild.name}"
